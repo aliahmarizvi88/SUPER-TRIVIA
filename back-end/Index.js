11dfcc700437ef788a1c0bf17b43e8dd7d8c14d5
Ali Ahmad <aliahmadrizvi88@gmail.com>
2025-05-14 15:05:14 +0500

    version 1.0

--- back-end/Index.js
+++ back-end/Index.js
@@ -1,19 +1,20 @@
+require('dotenv').config();
 const express = require('express');
 const scoreRoute = require('./routes/scoreRoute');
 const { connectDB } = require('./connection');
 const cors = require('cors');
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-connectDB('mongodb://127.0.0.1:27017/trivia_db')
-  .then(() => console.log('DB CONNCETION SUCCESSFULL'))
-  .catch((err) => console.log(`${err}, DB CONNCETION UNSUCESSFULL`));
+connectDB(process.env.MONGO_URI)
+  .then(() => console.log('DB CONNECTION SUCCESSFUL'))
+  .catch((err) => console.log(`${err}, DB CONNECTION UNSUCCESSFUL`));
 
 app.use('/api', scoreRoute);
 
-app.listen(PORT, () => console.log(`CONNCETION SUCCESSFULL ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
